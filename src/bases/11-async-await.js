dbb3a38e451e89def5407d42f8b94ab7c2aea3e5
Manuel Martin Jukisz <manujukisz@outlook.com>
2021-08-12 14:13:01 +0100

    Async y await practica para interderlo

---
+++ src/bases/11-async-await.js
@@ -0,0 +1,44 @@
+
+
+const getImagen = async() => {
+    
+    try {
+        const apiKey = 'QT5p4l2jUTQTlzi3qPqzaDzKsGZmbn5w';
+        const resp = await fetch(`https://api.giphy.com/v1/gifs/random?api_key=${apiKey}`);
+        const {data} = await resp.json()
+        
+        const { url } = data.images.original
+
+        const img = document.createElement('img');
+
+        img.src = url;
+
+        document.body.append(img)
+    } catch (error) {
+        console.error(error)
+    }
+}
+
+
+
+getImagen();
+
+
+
+
+// const peticion = fetch(`https://api.giphy.com/v1/gifs/random?api_key=${apiKey}`);
+
+//     peticion.then(resp => resp.json() )
+//         .then( ( { data } ) => {
+//             const { url } = data.images.original
+
+//             const img = document.createElement('img');
+//             img.src = url;
+
+//             document.body.append(img)
+//         }) 
+//         .catch(console.warn)
+
+
+
+
